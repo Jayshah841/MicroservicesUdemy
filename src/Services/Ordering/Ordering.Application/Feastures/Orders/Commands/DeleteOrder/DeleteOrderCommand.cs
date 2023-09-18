071022bd27eaf68f053aa3a7d5a5f3bf676a90d1
Jay Shah <Jay Shah@DESKTOP-MHGTDAD>
2023-09-19 02:39:42 +0530

    Section 7 Ordering API with Sql Server DB

---
+++ src/Services/Ordering/Ordering.Application/Feastures/Orders/Commands/DeleteOrder/DeleteOrderCommand.cs
@@ -0,0 +1,9 @@
+﻿using MediatR;
+
+namespace Ordering.Application.Feastures.Orders.Commands.DeleteOrder
+{
+    public class DeleteOrderCommand : IRequest
+    {
+        public int Id { get; set; }
+    }
+}
---
+++ src/Services/Ordering/Ordering.Infrastructure/Persistence/OrderContextSeed.cs
@@ -0,0 +1,26 @@
+﻿using Microsoft.Extensions.Logging;
+using Ordering.Domain.Entities;
+
+namespace Ordering.Infrastructure.Persistence
+{
+    public class OrderContextSeed
+    {
+        public static async Task SeedAsync(OrderContext context, ILogger<OrderContextSeed> logger)
+        {
+            if (!context.Orders.Any())
+            {
+                context.Orders.AddRange(GetPreconfiguredOrders());
+                await context.SaveChangesAsync();
+                logger.LogInformation("Seed database associated with context {DbContextName}", typeof(OrderContext).Name);
+            }
+        }
+
+        private static IEnumerable<Order> GetPreconfiguredOrders()
+        {
+            return new List<Order>
+            {
+                new Order() {UserName = "swn", FirstName = "Jay", LastName = "Shah", EmailAddress = "jayshah841@gmail.com", AddressLine = "Mumbai", Country = "India", TotalPrice = 350 }
+            };
+        }
+    }
+}
